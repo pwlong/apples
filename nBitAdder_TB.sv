e449d7c3c3facb43a9549ae1501d89aff8338244
Paul <paul@thelongs.ws>
2015-04-01 14:02:38 -0700

    TB and module working, yay!

--- nBitAdder_TB.sv
+++ nBitAdder_TB.sv
@@ -14,13 +14,14 @@
 
 
 
-module nBitAdderTestBench ();
+module nBitAdder_TB ();
 	
 	reg		[3:0]	a, b, s;
 	reg			 	c_in, c_out;
 	
 	integer i, j, k;				// loop counters
-	integer errors = 0;				// errors counter
+	integer f;							// file handle
+	integer errors = 0;			// errors counter
 
 	// instantiate the DUT
 	CarryLookAheadAdder4Bit #( /* no parameters*/)
@@ -33,27 +34,39 @@ module nBitAdderTestBench ();
 	);
 
 	initial begin
+		// open a logfile
+		f = $fopen("addertest.log","w");
+		$fwrite(f, "  Starting Simulation\n");
+		$fwrite(f, "=========================\n");
+
+		// build the input stimulus and compare to expectations
+		// this small solution space allows for an exhaustive test
 		for (i=0; i < 16; i=i+1) begin
+			//$fwrite(f, "into i loop!\n");
 			for (j=0; j < 16; j=j+1) begin
-				for (k=0; k==1; k=k+1) begin
+				//$fwrite(f, "into j loop\n");
+				for (k=0; k < 2; k=k+1) begin
+					//$fwrite(f, "into k loop");
 					#100 a = i;
 						 b = j;
 						 c_in = k;
-						 
-					#300 if ((a + b) != {c_out,s}) begin
+					#300 if ((a + b + c_in) != {c_out,s}) begin
 							errors = errors + 1;
-							$display("");
-							$display("  ERROR: %4d + %4d != %5d", a, b, {c_out,s});
+							$fwrite(f, "  ERROR: %2d + %2d + %1d != %2d\n", a, b, c_in, {c_out,s});
+					end
+					else begin
+						$fwrite(f, "  %2d + %2d + %1d = %2d\n", a, b, c_in, {c_out, s});
+					end
 				end
 			end
 		end
 		
-		$display("");
-		$display("  Simulation Complete");
-		$display("===================================================");
-		if (0 == errors) $display("  Congratulations on an error-free simulation.");
-		else             $display("  %4d errors detected", errors);
-    $display("");
+		$fwrite(f, "=========================\n");
+		$fwrite(f, "  Simulation Complete\n");
+		if (0 == errors) $fwrite(f, "  Congratulations on an\n  error-free simulation!\n");
+		else             $fwrite(f, "  %4d errors detected\n", errors);
+		$fwrite(f, "=========================\n");
+		$fclose(f);
 		$finish();
 	end
 endmodule
