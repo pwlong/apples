9849cf03799ed5399183da011640b88786420be2
Paul Long <github@thelongs.ws>
2015-04-01 11:24:06 -0700

    Added first crack at TB and code cleanup on adder

--- 4-bitCarryLookAheadAdder.sv
+++ 4-bitCarryLookAheadAdder.sv
@@ -1,7 +1,32 @@
+`timescale 1ns\1ps
+/*!************************************************************************************************
+*
+*	@file		4-bitCarryLookAheadAdder.sv
+*
+*	@author		Paul Long <paul@thelongs.ws>
+*	@date		31 March 2015
+*	@copyright	Paul Long, 2015
+*
+*	@brief		4-bit expandable carry lookahead adder
+*				
+*				This module implements a 4-bit carry lookahead adder as described in  ECE571
+*				at Portland State University, Spring 2015. It uses XOR for the addition and 
+*				constructs generate and propagate signals for the lookahead logic. The lookahead
+*				algorithm can be summarized thusly: a carry out (CO) is produced if the current
+*				stage generates a CO (i.e. a=b=1) or if any previous stage generates a CO which
+*				propagates through all intervening stages including the current stage.
+*				
+*				Inputs:	 a,b   4-bit numbers to be added
+*						 c_in  carry in from any previous stages (module is expandable)
+*					
+*				Outputs: s     4-bit result of the addition
+*						 c_out carry out to the next stage
+*
+*				Propagation delay is modelled
+*			
+**************************************************************************************************/
 
-
-
-
+	
 module CarryLookAheadAdder4Bit (
 	input  [3:0] a,
 	input  [3:0] b,
@@ -20,27 +45,27 @@ module CarryLookAheadAdder4Bit (
 	
 	generate
 		// lookahead logic
-		assign carry[0] = c_in;
-		assign carry[1] = g[0] | 
-						  c_in & p[0];
-		assign carry[2] = g[1] | 
-						  g[0] & p[1] | 
-						  c_in & p[0] & p[1];
-		assign carry[3] = g[2] | 
-						  g[1] & p[2] | 
-						  g[0] & p[1] & p[2] | 
-						  c_in & p[0] & p[1] & p[2];
-		assign c_out    = g[3] | 
-						  g[2] & p[3] | 
-						  g[1] & p[2] & p[3] | 
-						  g[0] & p[1] & p[2] & p[3] | 
-						  c_in & p[0] & p[1] & p[2] & p[3];
+		assign #5 carry[0] = c_in;
+		assign #5 carry[1] = g[0] | 
+							 c_in & p[0];
+		assign #5 carry[2] = g[1] | 
+							 g[0] & p[1] | 
+							 c_in & p[0] & p[1];
+		assign #5 carry[3] = g[2] | 
+							 g[1] & p[2] | 
+							 g[0] & p[1] & p[2] | 
+							 c_in & p[0] & p[1] & p[2];
+		assign #5 c_out    = g[3] | 
+							 g[2] & p[3] | 
+							 g[1] & p[2] & p[3] | 
+							 g[0] & p[1] & p[2] & p[3] | 
+							 c_in & p[0] & p[1] & p[2] & p[3];
 		
 		// adder logic
 		for (ii = 0; ii < 4; ii = ii + 1) begin
-			assign g[ii] = a[ii] & b[ii];
-			assign p[ii] = a[ii] | b[ii];
-			assign s[ii] = a[ii] ^ b[ii] ^ carry[ii];
+			assign #5 g[ii] = a[ii] & b[ii];
+			assign #5 p[ii] = a[ii] | b[ii];
+			assign #5 s[ii] = a[ii] ^ b[ii] ^ carry[ii];
 		end
 	endgenerate
 	
---
+++ nBitAdder_TB.sv
@@ -0,0 +1,64 @@
+`timescale 1ns\1ps
+/*!************************************************************************************************
+*
+*	@file		nBitAdder_TB.sv
+*
+*	@author		Paul Long <paul@thelongs.ws>
+*	@date		31 March 2015
+*	@copyright	Paul Long, 2015
+*
+*	@brief		Test bench 4-bit expandable carry lookahead adder
+*
+*
+**************************************************************************************************/
+
+
+
+module nBitAdderTestBench ();
+	
+	reg		[3:0]	a, b, s;
+	reg			 	c_in, c_out;
+	
+	integer i, j, k;				// loop counters
+	integer errors = 0;				// errors counter
+
+	// instantiate the DUT
+	CarryLookAheadAdder4Bit #( /* no parameters*/)
+	adder (
+		.a		(a),
+		.b		(b),
+		.c_in	(c_in),
+		.s		(s),
+		.c_out	(c_out)
+	);
+
+	initial begin
+		for (i=0; i < 16; i=i+1) begin
+			for (j=0; j < 16; j=j+1) begin
+				for (k=0, k==1, k=k+1) begin
+					#100 a = i;
+						 b = j;
+						 c_in = k;
+						 
+					#300 if ((a + b) != {c_out,s}) begin
+							errors = errors + 1;
+							$display("");
+							$display("  ERROR: %4d + %4d != %5d", a, b, {c_out,s})
+				end
+			end
+		end
+		
+		$display("");
+		$display("  Simulation Complete");
+		$display("===================================================");
+		if (0 == errors) begin
+			$display("  Congratulations on an error-free simulation.");
+        end
+		else begin
+             $display("  %4d errors detected", errors);
+        end
+             
+        $display("");
+		$finish();
+	end
+end module
